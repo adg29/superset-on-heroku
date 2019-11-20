56f6a905f0aafcdeeb0e198000ac8a68371890a0
Nicholas Wittstruck <nicholas.wittstruck@wertfaktor.de>
2019-11-20 10:57:38 +0100

    updates version

--- superset_config.py
+++ superset_config.py
@@ -1,25 +1,30 @@
-import os
 #---------------------------------------------------------
 # Superset specific config
 #---------------------------------------------------------
-# ROW_LIMIT = 5000
-SUPERSET_WORKERS = 1 # for it to work in heroku basic/hobby dynos increase as you like
+ROW_LIMIT = 5000
 
-# SUPERSET_WEBSERVER_PORT = 8088
+SUPERSET_WEBSERVER_PORT = 8088
 #---------------------------------------------------------
 
 #---------------------------------------------------------
 # Flask App Builder configuration
 #---------------------------------------------------------
 # Your App secret key
-SECRET_KEY = '\2\1thisismyscretkey\1\2\f\z\z\i'  # noqa
+SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
 
 # The SQLAlchemy connection string to your database backend
 # This connection defines the path to the database that stores your
-# Superset metadata (slices, connections, tables, dashboards, ...).
+# superset metadata (slices, connections, tables, dashboards, ...).
 # Note that the connection information to connect to the datasources
 # you want to explore are managed directly in the web UI
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI =  os.environ['DATABASE_URL']
 
 # Flask-WTF flag for CSRF
-CSRF_ENABLED = True
+WTF_CSRF_ENABLED = True
+# Add endpoints that need to be exempt from CSRF protection
+WTF_CSRF_EXEMPT_LIST = []
+# A CSRF token that expires in 1 year
+WTF_CSRF_TIME_LIMIT = 60 * 60 * 24 * 365
+
+# Set this API key to enable Mapbox visualizations
+MAPBOX_API_KEY = ''
